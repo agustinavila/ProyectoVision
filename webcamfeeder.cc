3b8ffcc09b5c9a03c182be7313bd00291c5f91d3
Agustin Avila <tinto.avila@gmail.com>
2021-06-17 19:57:05 -0300

    Generada la clase concreta WebcamFeeder

---
+++ webcamfeeder.cc
@@ -0,0 +1,51 @@
+/**
+ * @file webcamfeeder.cc
+ * @author Agustin Avila (tinto.avila@gmail.com)
+ * @brief 
+ * @version 0.1
+ * @date 2021-06-17
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+#include "feeder.h"
+#include "webcamfeeder.h"
+
+/**
+ * @brief Construye un nuevo objeto de la clase Webcam Feeder:: Webcam Feeder
+ * @details Puede tomar como parametro \p idx, que es el indice de la webcam.
+ * @param idx 
+ */
+WebcamFeeder::WebcamFeeder(int idx)
+{
+	cout << "abriendo webcam";
+	if (!this->cap.open(idx))
+    {
+        cout << "No se detectó camara, verifique conexion"<<endl;
+     
+    }
+    this->cap.set(CV_CAP_PROP_FRAME_WIDTH, 1024);
+    this->cap.set(CV_CAP_PROP_FRAME_HEIGHT, 768);
+}
+
+/**
+ * @brief Destruye el objeto de la clase Webcam Feeder:: Webcam Feeder
+ * 
+ */
+WebcamFeeder::~WebcamFeeder()
+{
+	cout << "Liberando Webcam...";
+	this->cap.release();
+	cout << " webcam liberada"<<endl;
+}
+
+/**
+ * @brief Sobrecarga del operador Feeder:getFrame(). Cuando se llama, devuelve el proximo frame de la webcam
+ * 
+ * @return Mat 
+ */
+Mat WebcamFeeder::getFrame()
+{
+    this->cap >> this->frame;
+    return this->frame;
+}
---
+++ webcamfeeder.h
@@ -0,0 +1,38 @@
+/**
+ * @file webcamfeeder.h
+ * @author Agustin Avila (tinto.avila@gmail.com)
+ * @brief 
+ * @version 0.1
+ * @date 2021-06-17
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+#include "feeder.h"
+#include <opencv2/videoio/videoio_c.h>
+#include <opencv2/imgproc/types_c.h>
+#include <opencv2/video/video.hpp>
+#include <opencv2/videoio.hpp>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+using namespace cv;
+
+
+/**
+ * @brief Sobrecarga de la clase Feeder para abrir una webcam
+ * 
+ */
+class WebcamFeeder : public Feeder
+{
+private:
+    Mat frame;
+    int idx;
+    cv::VideoCapture cap;
+public:
+	Mat getFrame();
+	WebcamFeeder(int idx = 0);
+	virtual ~WebcamFeeder() override;
+};
