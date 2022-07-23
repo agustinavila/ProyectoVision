58c9f3777bbfbdfa27bcedfa3e42762f30495214
agustinavila <47158942+agustinavila@users.noreply.github.com>
2022-07-23 11:45:51 +0000

    Deploying to gh-pages from  @ d36d7209de2a563fa685fa36eb01fa82a81e9054 🚀

--- class_analizador_landmarks.js
+++ class_analizador_landmarks.js
@@ -3,7 +3,7 @@ var class_analizador_landmarks =
     [ "AnalizadorLandmarks", "class_analizador_landmarks.html#aa9b92ebc59a1cf29015e2b7301e5219b", null ],
     [ "~AnalizadorLandmarks", "class_analizador_landmarks.html#a5ecc1c774b6e74f4d1346010bb3663d4", null ],
     [ "calcularAngulo", "class_analizador_landmarks.html#acfe41c143ac1af3f6dff00945cd56319", null ],
-    [ "calcularAsimetria", "class_analizador_landmarks.html#a5f625cf208aabecb0e2ffb06465d9c74", null ],
+    [ "calcularAsimetria", "class_analizador_landmarks.html#ad4b36bb17e56a458e78a40eef71e96ca", null ],
     [ "calcularMax", "class_analizador_landmarks.html#aa19ac2b873fc9d509365e3cec5729c9c", null ],
     [ "calcularPendiente", "class_analizador_landmarks.html#aeddef32bfc58efa693d8ff475b86d339", null ],
     [ "getLandmarks", "class_analizador_landmarks.html#a11765325876194183597fcee9dc2c9ec", null ],
